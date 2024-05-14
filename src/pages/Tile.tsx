ab74cd86546f85e16c273ecc8527605dbe42bc42
Jose Oliveros <chinnno15@gmail.com>
2024-05-14 16:34:44 -0600

    add tile controls
    Signed-off-by: Jose Oliveros <chinnno15@gmail.com>

--- src/pages/Tile.tsx
+++ src/pages/Tile.tsx
@@ -1,3 +1,11 @@
+import {
+  ArrowLeftCircleIcon, 
+  ArrowDownCircleIcon,
+  ArrowRightCircleIcon,
+  ArrowUpCircleIcon
+} from '@heroicons/react/24/solid'
+import { useState } from 'react';
+
 interface TileProps {
   imageUrl: string;
   index?: number;
@@ -9,8 +17,9 @@ export const Tile: React.FC<TileProps> = ({
   imageUrl,
   index = 0,
   baseImageHeight,
-  baseImageWidth,
+  baseImageWidth
 }) => {
+  const [controlsEnabled, setControlsEnabled] = useState(false);
   const tileWidth = -100;
   let minImageDim = 0;
 
@@ -68,5 +77,28 @@ export const Tile: React.FC<TileProps> = ({
   //   ...bgProps
   // }, null, 2))
 
-  return <div className='tile' style={bgProps} id={`tile-${index}`}></div>;
+  return (
+    <div className='relative z-0' 
+    onMouseEnter={()=>setControlsEnabled(true)}
+    onMouseLeave={()=>setControlsEnabled(false)}>
+      {controlsEnabled ? (
+        <div>
+          <div className='absolute top-10 left-1 flex z-10'>
+            <ArrowLeftCircleIcon className='text-gray-500 h-6 w-6'></ArrowLeftCircleIcon>
+          </div>
+          <div className='absolute bottom-1 right-10 flex z-10'>
+            <ArrowDownCircleIcon className='text-gray-500 h-6 w-6'></ArrowDownCircleIcon>
+          </div>
+          <div className='absolute right-1 top-10 flex z-10'>
+            <ArrowRightCircleIcon className='text-gray-500 h-6 w-6'></ArrowRightCircleIcon>
+          </div>
+          <div className='absolute top-1 right-10 flex z-10'>
+            <ArrowUpCircleIcon className='text-gray-500 h-6 w-6'></ArrowUpCircleIcon>
+          </div>
+        </div>
+      ):(<></>)}
+
+      <div className='tile' style={bgProps} id={`tile-${index}`}></div>
+    </div>
+  );
 };
